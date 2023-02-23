58a125293dc17855b287e3df2a4c388bb89a2b3d
Justus Voss <justusvoss27@gmail.com>
2023-02-22 21:17:50 -0800

    Updated for input_path_full

--- file_converter/app.py
+++ file_converter/app.py
@@ -6,20 +6,20 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def convert_file():
     if request.method == 'POST':
-        input_path = request.form['input_path']
-        output_path = request.form['output_path']
+        input_path_full = request.form['input_path']
+        output_path_full = request.form['output_path']
         input_format = request.form['input_format']
         output_format = request.form['output_format']
 
-        cmd = ['python', 'convert_file.py', input_path, output_path, input_format, output_format]
+        cmd = ['python', 'convert_file.py', input_path_full, output_path_full, input_format, output_format]
         result = subprocess.run(cmd, capture_output=True, text=True)
 
         if result.returncode == 0:
             output = result.stdout
         else:
             output = result.stderr
-            
+
         if output_format == 'json':
-            return send_file(output_path, as_attachment=True)
+            return send_file(output_path_full, as_attachment=True)
 
     return render_template('index.html')
--- file_converter/convert_file.py
+++ file_converter/convert_file.py
@@ -4,8 +4,8 @@ import pandas as pd
 
 # Define the command line arguments
 parser = argparse.ArgumentParser(description='Convert file format.')
-parser.add_argument('input_path', type=str, help='Path to input file')
-parser.add_argument('output_path', type=str, help='Path to output file')
+parser.add_argument('input_path_full', type=str, help='Path to input file')
+parser.add_argument('output_path_full', type=str, help='Path to output file')
 parser.add_argument('input_format', type=str, help='Input file format')
 parser.add_argument('output_format', type=str, help='Output file format')
 
@@ -14,16 +14,16 @@ args = parser.parse_args()
 
 # Read the input file
 if args.input_format == 'csv':
-    df = pd.read_csv(args.input_path)
+    df = pd.read_csv(args.input_path_full)
 elif args.input_format == 'json':
-    with open(args.input_path, 'r') as f:
+    with open(args.input_path_full, 'r') as f:
         data = json.load(f)
     df = pd.DataFrame(data)
 
 # Convert the data to the output format
 if args.output_format == 'csv':
-    df.to_csv(args.output_path, index=False)
+    df.to_csv(args.output_path_full, index=False)
 elif args.output_format == 'json':
-    with open(args.output_path, 'w') as f:
+    with open(args.output_path_full, 'w') as f:
         data = df.to_dict(orient='records')
         json.dump(data, f, indent=4)
